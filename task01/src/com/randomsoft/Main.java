eddbd17f2b8fe4e02b988314c4d0712d5281620c
Destinat0r <ashavrn@protonmail.com>
2019-11-21 12:36:11 +0200

    added comment

--- task01/src/com/randomsoft/Main.java
+++ task01/src/com/randomsoft/Main.java
@@ -3,6 +3,8 @@ package com.randomsoft;
 public class Main {
 
     public static void main(String[] args) {
+
+        /* keywords could be taken from config or any other source */
         Model model = new Model("Hello", "Java!");
         View view = new View();
 
