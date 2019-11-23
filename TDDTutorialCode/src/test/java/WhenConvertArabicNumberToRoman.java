89a5d5a6d41819a5e408348eeea4003345063cd2
Destinat0r <ashavrn@protonmail.com>
2019-11-23 12:34:08 +0200

    added test for 0

--- TDDTutorialCode/src/test/java/WhenConvertArabicNumberToRoman.java
+++ TDDTutorialCode/src/test/java/WhenConvertArabicNumberToRoman.java
@@ -1,4 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class WhenConvertArabicNumberToRoman {
-    
+
+    @Test
+    public void convert_0_To_EmptyString() {
+        ArabicToRomanNumberConverter converter = new ArabicToRomanNumberConverter();
+        String romanNumber = converter.convert(0);
+
+        assertEquals("", romanNumber);
+    }
 }
