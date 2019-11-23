a7fd4e350ed0c868b230df98232798ee98740921
Destinat0r <ashavrn@protonmail.com>
2019-11-23 20:08:33 +0200

    removed unused methods

--- TDDTutorialCode/src/main/java/ArabicToRomanNumberConverter.java
+++ TDDTutorialCode/src/main/java/ArabicToRomanNumberConverter.java
@@ -32,40 +32,12 @@ public class ArabicToRomanNumberConverter {
         return arabicToRoman;
     }
 
-    private int appendHundreds(int arabicNumber, StringBuilder builder) {
-        return appendSymbolsForNumber(100, "C", arabicNumber, builder);
-    }
-
     private int appendSymbolsForNumber(int number, String symbol, int arabicNumber, StringBuilder builder) {
         while (arabicNumber >= number) {
             builder.append(symbol);
             arabicNumber -= number;
         }
         return arabicNumber;
-    }
-
-    private int appendFiftyIfNeeded(int arabicNumber, StringBuilder builder) {
-        return appendSymbolsForNumber(50, "L", arabicNumber, builder);
-    }
-
-    private int appendNineIfNeeded(int arabicNumber, StringBuilder builder) {
-        return appendSymbolsForNumber(9, "IX", arabicNumber, builder);
-    }
-
-    private int appendFourIfNeeded(int arabicNumber, StringBuilder builder) {
-        return appendSymbolsForNumber(4, "IV", arabicNumber, builder);
-    }
-
-    private int appendFiveIfNeeded(int arabicNumber, StringBuilder builder) {
-        return appendSymbolsForNumber(5, "V", arabicNumber, builder);
-    }
-
-    private int appendDozens(int arabicNumber, StringBuilder builder) {
-        return appendSymbolsForNumber(10, "X", arabicNumber, builder);
-    }
-
-    private int appendOnes(int arabicNumber, StringBuilder builder) {
-        return appendSymbolsForNumber(1, "I", arabicNumber, builder);
     }
 
     private String getDefault() {
