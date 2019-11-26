ddb0db7a1973a80e91bf1c8ea5ff2e8bd276dd4f
Destinat0r <ashavrn@protonmail.com>
2019-11-26 16:37:53 +0200

    created Subscriber class and fields inside it

---
+++ task03_registration_form/src/model/Subscriber.java
@@ -0,0 +1,30 @@
+package model;
+
+import java.util.Date;
+
+public class Subscriber {
+    private String firstName;
+    private String patronymic;
+    private String lastName;
+    private String formattedName;
+
+    private String nickname;
+    private String comment;
+
+    private String phone;
+    private String mobileOne;
+    private String mobileTwo;
+    private String skype;
+
+    private String index;
+    private String city;
+    private String street;
+    private String building;
+    private String apartment;
+    private String fullAddress;
+
+    private Date created;
+    private Date modified;
+
+    
+}
