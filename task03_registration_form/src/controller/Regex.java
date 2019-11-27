7502141e68b6de68a8f4f2ae706fd346c934088c
Destinat0r <ashavrn@protonmail.com>
2019-11-27 10:03:38 +0200

    interfaces for regex constants and for form input messages

---
+++ task03_registration_form/src/controller/Regex.java
@@ -0,0 +1,29 @@
+package controller;
+
+public interface Regex {
+
+
+    String name = "^[а-яА-ЯёЁa-zA-Z]+$";
+    String lastName = name;
+    String firstName = name;
+    String patronymic = "^([а-яА-ЯёЁa-zA-Z]+)?$";
+    String nickname = "^[a-zA-Z][a-zA-Z0-9-_\\.]{1,20}$ ";
+    String comment = "^[а-яА-ЯёЁa-zA-Z0-9]{,50}$";
+    String homePhone = "\\+38044\\d{7}";
+    String mobileOne = "\\+380(66|50|95|63|73|93|96|97|68)\\d{7}";
+    String mobileTwo = "^(\\+380(66|50|95|63|73|93|96|97|68)\\d{7})?$";
+    String email = "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@"
+                           + "(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?";
+
+    String skype = "^([a-zA-Z][a-zA-Z0-9]{5,31})?$";
+    String index = "\\d{5}";
+    String city = "[a-zA-Zа-яА-ЯёЁ]{3,}";
+    String street = "^[а-яА-ЯёЁa-zA-Z]+$";
+    String building = "\\d{1,3}[а-яА-ЯёЁa-zA-Z]?";
+    String apartment = "\\d{1,3}";
+
+
+    String[] patterns = {lastName, firstName, patronymic, nickname, comment, homePhone, mobileOne, mobileTwo, email,
+                            skype, index, city, street, building, apartment};
+
+}
---
+++ task03_registration_form/src/view/FieldMessages.java
@@ -0,0 +1,32 @@
+package view;
+
+public interface FieldMessages {
+    String firstName = "Please, enter your first name (1-50 characters)";
+    String patronymic = "Please, enter your patronymic name if any (1-50 characters), else press 'Enter'.";
+    String lastName = "Please, enter your last name (1-50 characters).";
+
+    String nickname = "Please, enter your nickname (1-50 characters).";
+    String comment = "If you like, enter a comment (1-50 characters), or press 'Enter'.";
+
+    String homePhone = "Please, enter your city phone number you could be contacted with (use pattern +38044...), "
+                               + "if none press 'Enter'.";
+    String mobileOne = "Please, enter your mobile phone number (use pattern +380... ).";
+    String mobileTwo = "If you like, enter second mobile phone number (use pattern +380... ), else press 'Enter'.";
+    String email = "Please, enter your email.";
+    String skype = "Enter your skype account name, or press 'Enter'.";
+
+    String index = "Please, enter your address.\nPlease, enter index.";
+    String city = "Please, enter city name.";
+    String street = "Please, enter street.";
+    String building = "Please, enter building number.";
+    String apartment = "Please, enter apartment number.";
+
+    String invalidInput = "INVALID INPUT!";
+
+    String[] messages = {lastName, firstName, patronymic, nickname, comment, homePhone, mobileOne, mobileTwo, email,
+            skype, index, city, street, building, apartment};
+
+    //    String created;
+//    String modified;
+
+}
