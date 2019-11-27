08984176e488361dc148b69222c332780c2b3ea8
Destinat0r <ashavrn@protonmail.com>
2019-11-27 14:14:02 +0200

    added 98 mobile code to mobile regex

--- task03_registration_form/src/controller/Regex.java
+++ task03_registration_form/src/controller/Regex.java
@@ -10,7 +10,7 @@ public interface Regex {
     String nickname = "^[a-zA-Z][a-zA-Z0-9]*$";
     String comment = "^[а-яА-ЯёЁa-zA-Z0-9\\s]{0,50}$";
     String homePhone = "^(\\+38044\\d{7})?$";
-    String mobileOne = "\\+380(66|50|95|63|73|93|96|97|68)\\d{7}";
+    String mobileOne = "\\+380(66|50|95|63|73|93|96|97|68|98)\\d{7}";
     String mobileTwo = "^(\\+380(66|50|95|63|73|93|96|97|68)\\d{7})?$";
     String email = "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@"
                            + "(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?";
