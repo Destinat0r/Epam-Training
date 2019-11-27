3e77ab5d28866766e2903fdddddf3db0d049b679
Destinat0r <ashavrn@protonmail.com>
2019-11-27 10:54:55 +0200

    singled out prompting user to promptUserUntilValidInput(), made promptSubscriberInfo() static

--- task03_registration_form/src/controller/UserInputReader.java
+++ task03_registration_form/src/controller/UserInputReader.java
@@ -2,25 +2,35 @@ package controller;
 
 import view.FieldMessages;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class UserInputReader {
 
-
-    public void promptSubscriberInfo() {
+    public static ArrayList<String> promptSubscriberInfo() {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> allSubscriberInfo = new ArrayList<>();
 
         int numberOfFields = FieldMessages.messages.length;
 
         for (int i = 0; i < numberOfFields; i++) {
             System.out.println(FieldMessages.messages[i]);
-            String userInput = scanner.nextLine();
 
-            while (!userInput.matches(Regex.patterns[i])) {
-                System.out.println(FieldMessages.invalidInput);
-                System.out.println(FieldMessages.messages[i]);
-                userInput = scanner.nextLine();
-            }
+            allSubscriberInfo.add(promptUserUntilValidInput(scanner, i));
         }
+        return  allSubscriberInfo;
     }
+
+    private static String promptUserUntilValidInput(Scanner scanner, int messageCounter) {
+        String userInput = scanner.nextLine();
+
+        while (!userInput.matches(Regex.patterns[messageCounter])) {
+            System.out.println(FieldMessages.invalidInput);
+            System.out.println(FieldMessages.messages[messageCounter]);
+            userInput = scanner.nextLine();
+        }
+
+        return userInput;
+    }
+
 }
