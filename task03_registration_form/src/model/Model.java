c6a4b50fba044b9a8143bb134ac76aa58d873713
Destinat0r <ashavrn@protonmail.com>
2019-11-27 11:04:18 +0200

    added subscriber, constructor, getter and setter

--- task03_registration_form/src/model/Model.java
+++ task03_registration_form/src/model/Model.java
@@ -1,5 +1,17 @@
 package model;
 
 public class Model {
-    
+    private Subscriber subscriber;
+
+    public Model() {
+        this.subscriber = new Subscriber();
+    }
+
+    public Subscriber getSubscriber() {
+        return subscriber;
+    }
+
+    public void setSubscriber(Subscriber subscriber) {
+        this.subscriber = subscriber;
+    }
 }
