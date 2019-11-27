13c80c79396229d26855c3d26a3d4781763a9029
Destinat0r <ashavrn@protonmail.com>
2019-11-27 12:33:34 +0200

    changed comment to have spaces, made homePhone optional, allowed spaces for street

--- task03_registration_form/src/controller/Regex.java
+++ task03_registration_form/src/controller/Regex.java
@@ -8,8 +8,8 @@ public interface Regex {
     String firstName = name;
     String patronymic = "^([а-яА-ЯёЁa-zA-Z]+)?$";
     String nickname = "^[a-zA-Z][a-zA-Z0-9]*$";
-    String comment = "^[а-яА-ЯёЁa-zA-Z0-9]{0,50}$";
-    String homePhone = "\\+38044\\d{7}";
+    String comment = "^[а-яА-ЯёЁa-zA-Z0-9\\s]{0,50}$";
+    String homePhone = "^(\\+38044\\d{7})?$";
     String mobileOne = "\\+380(66|50|95|63|73|93|96|97|68)\\d{7}";
     String mobileTwo = "^(\\+380(66|50|95|63|73|93|96|97|68)\\d{7})?$";
     String email = "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@"
@@ -18,7 +18,7 @@ public interface Regex {
     String skype = "^([a-zA-Z][a-zA-Z0-9]{5,31})?$";
     String index = "\\d{5}";
     String city = "[a-zA-Zа-яА-ЯёЁ]{3,}";
-    String street = "^[а-яА-ЯёЁa-zA-Z]+$";
+    String street = "^[а-яА-ЯёЁa-zA-Z\\s]+$";
     String building = "\\d{1,3}[а-яА-ЯёЁa-zA-Z]?";
     String apartment = "\\d{1,3}";
 
