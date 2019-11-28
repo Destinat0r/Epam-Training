f3b444823fc6de784349cada47d61c6715039f2f
Destinat0r <ashavrn@protonmail.com>
2019-11-28 17:57:48 +0200

    added printMessage(), concat(), getter and setter for messageBundle

--- task03_registration_form/src/view/View.java
+++ task03_registration_form/src/view/View.java
@@ -2,31 +2,33 @@ package view;
 
 import java.util.Locale;
 import java.util.ResourceBundle;
-import java.util.Scanner;
 
 public class View {
 
-    private ResourceBundle messageBundle;
+    private ResourceBundle messagesBundle;
 
     public View() {
-        messageBundle = ResourceBundle.getBundle("messages", new Locale("en"));
-    }
-
-    public void promptForLocale(Scanner scanner) {
-        printMessage("prompt_for_locale");
-        String userInput = scanner.nextLine();
-
-        while (!userInput.equals("ua") || !userInput.equals("")) {
-            printMessage("unknown_locale");
-            userInput = scanner.nextLine();
-        }
-
-        if (userInput.equals("ua")) {
-            messageBundle = ResourceBundle.getBundle("messages", new Locale("ua", "UA"));
-        }
+        messagesBundle = ResourceBundle.getBundle("messages", new Locale("en"));
     }
 
     public void printMessage(String message) {
-        System.out.println(messageBundle.getString(message));
+        System.out.println(messagesBundle.getString(message));
+    }
+
+    public String concat(String ...strings) {
+        StringBuilder builder = new StringBuilder();
+
+        for (String string : strings) {
+            builder.append(string);
+        }
+        return builder.toString();
+    }
+
+    public ResourceBundle getMessagesBundle() {
+        return messagesBundle;
+    }
+
+    public void setMessagesBundle(ResourceBundle messagesBundle) {
+        this.messagesBundle = messagesBundle;
     }
 }
