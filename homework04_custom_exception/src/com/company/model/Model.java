adc92e9caa99aa458a3dc22f1271e3084ab92fc6
Destinat0r <ashavrn@protonmail.com>
2019-12-08 19:36:23 +0200

    added hashset of logins, addNewLogin()

--- homework04_custom_exception/src/com/company/model/Model.java
+++ homework04_custom_exception/src/com/company/model/Model.java
@@ -1,7 +1,22 @@
 package com.company.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by student on 26.09.2017.
  */
 public class Model {
+    private Set<String> logins = new HashSet<String>() {{
+        add("login123");
+        add("vasia345");
+        add("tonia312");
+    }};
+
+    public void addNewLogin(String login) throws RecordAlreadyExistException {
+        if (logins.contains(login)) {
+            throw new RecordAlreadyExistException();
+        }
+        logins.add(login);
+    }
 }
