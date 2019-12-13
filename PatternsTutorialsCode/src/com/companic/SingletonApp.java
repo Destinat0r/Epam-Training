ef4cf9f592d0e9ed8702c547e58bc7a4d675ee1d
Destinat0r <ashavrn@protonmail.com>
2019-12-13 14:27:51 +0200

    added singleton pattern

---
+++ PatternsTutorialsCode/src/com/companic/SingletonApp.java
@@ -0,0 +1,50 @@
+package com.companic;
+
+public class SingletonApp {
+    public static void main(String[] args) throws InterruptedException {
+
+        final int SIZE = 1000;
+
+        Thread t[] = new Thread[SIZE];
+
+        for (int i = 0; i < SIZE; i++) {
+            t[i] = new Thread(new R());
+            t[i].start();
+        }
+
+        for (int i = 0; i < SIZE; i++) {
+            t[i].join();
+        }
+
+
+        System.out.println(Singleton.counter);
+
+    }
+
+}
+
+class R implements Runnable {
+
+    @Override public void run() {
+        Singleton.getInstance();
+    }
+}
+
+class Singleton {
+    public static int counter = 0;
+    private static Singleton instance;
+
+    private Singleton() {
+        counter++;
+    }
+
+    public static synchronized Singleton getInstance() {
+        if (instance == null) {
+             synchronized (Singleton.class) {
+                 instance = new Singleton();
+             }
+        }
+
+        return instance;
+    }
+}
