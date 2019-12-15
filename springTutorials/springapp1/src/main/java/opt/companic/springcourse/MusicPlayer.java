458712f43fa9d97cabd36350df0206242d78680d
Destinat0r <ashavrn@protonmail.com>
2019-12-15 19:58:40 +0200

    @Qualifier annotation

--- springTutorials/springapp1/src/main/java/opt/companic/springcourse/MusicPlayer.java
+++ springTutorials/springapp1/src/main/java/opt/companic/springcourse/MusicPlayer.java
@@ -1,42 +1,17 @@
 package opt.companic.springcourse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-//    @Autowired
-//    private Music music;
-
-    private ClassicalMusic classicalMusic;
-    private ElectronicMusic electronicMusic;
 
     @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, ElectronicMusic electronicMusic) {
-        this.classicalMusic = classicalMusic;
-        this.electronicMusic = electronicMusic;
-    }
-    //    @Autowired
-
-//    @Autowired
-//    public void setMusic(Music music) {
-//        this.music = music;
-//    }
+    @Qualifier("electronicMusic")
+    private Music music1;
 
     public String playMusic() {
-//        System.out.println("Playing " + classicalMusic.getSong());
-        return "Playing " + electronicMusic.getSong();
-    }
-
-    public ClassicalMusic getClassicalMusic() {
-        return classicalMusic;
-    }
-
-    public ElectronicMusic getElectronicMusic() {
-        return electronicMusic;
-    }
-
-    @Override public String toString() {
-        return "MusicPlayer{" + "classicalMusic=" + classicalMusic + ", electronicMusic=" + electronicMusic + '}';
+        return "Playing " + music1.getSong();
     }
 }
