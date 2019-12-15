e43c6faa742209156564cd1d6f561e8ca6783865
Destinat0r <ashavrn@protonmail.com>
2019-12-15 17:01:55 +0200

    bean init, destroy, singleton configs

--- springTutorials/springapp1/src/main/java/opt/companic/springcourse/TestSpring.java
+++ springTutorials/springapp1/src/main/java/opt/companic/springcourse/TestSpring.java
@@ -6,24 +6,28 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-//        Music music = context.getBean("musicBean", Music.class);
+        Music music = context.getBean("classicalMusic", Music.class);
+        Music music2 = context.getBean("classicalMusic", Music.class);
+        System.out.println(music.getSong());
 
-//        MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
+        //        MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        System.out.println(musicPlayer.getVolume());
-        System.out.println(musicPlayer.getName());
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer.playMusic();
+//
+//        System.out.println(musicPlayer.getVolume());
+//        System.out.println(musicPlayer.getName());
+//
+//        MusicPlayer anotherMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//        System.out.println(musicPlayer == anotherMusicPlayer);
+//        System.out.println(musicPlayer);
+//        System.out.println(anotherMusicPlayer);
+//
+//        musicPlayer.setVolume(100);
+//        System.out.println(anotherMusicPlayer.getVolume());
 
-        MusicPlayer anotherMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-
-        System.out.println(musicPlayer == anotherMusicPlayer);
-        System.out.println(musicPlayer);
-        System.out.println(anotherMusicPlayer);
-
-        musicPlayer.setVolume(100);
-        System.out.println(anotherMusicPlayer.getVolume());
 
         context.close();
     }
